003f1863cf959a3061db592e210438b7b4a2932d
George W Langham <georgelangham1990@gmail.com>
2021-05-18 15:29:14 +0100

    final behaviours

--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import DiscoverBlock from "./DiscoverBlock/components/DiscoverBlock";
 import makeRequest from "../api/makeRequest";
 import "../styles/_discover.scss";
-
+// if had time I'd probably refactor this into fucntional rather than class-based
+// possibly with a useReducer
 export default class Discover extends Component {
   constructor() {
     super();
@@ -27,13 +28,14 @@ export default class Discover extends Component {
     const responseCategories = await makeRequest("categories").catch((err) => {
       console.error(err);
     });
-    console.log({ responseCategories });
+    // As ui already has css for loading items, should be fine to load all in on mount
+    // could for design purposes only stagger the loading, but that should be handled with animation delay in css
     const newReleases = responseNewReleases?.data?.albums?.items;
-    const newPlaylists = responsePlaylists?.data?.playlists?.items;
+    const playlists = responsePlaylists?.data?.playlists?.items;
     const categories = responseCategories?.data?.categories?.items;
-    console.log({
+    this.setState({
       newReleases,
-      newPlaylists,
+      playlists,
       categories
     });
   }

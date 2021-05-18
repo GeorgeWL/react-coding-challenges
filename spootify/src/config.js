71d5dfcff281dd77d25afc4a79db7c70e94abcd6
George W Langham <georgelangham1990@gmail.com>
2021-05-18 15:21:27 +0100

    add helper default export. test with console.ligs

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -1,8 +1,8 @@
 export default {
   api: {
-    baseUrl: 'https://api.spotify.com/v1',
-    authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
+    baseUrl: "https://api.spotify.com/v1",
+    authUrl: "https://accounts.spotify.com/api/token",
+    clientId: "2bbf07a154024299bc67f1d6233d015f",
+    clientSecret: "ee55d95bce414732992598f6f5a3ff42"
   }
-}
+};
--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
-import '../styles/_discover.scss';
+import React, { Component } from "react";
+import DiscoverBlock from "./DiscoverBlock/components/DiscoverBlock";
+import makeRequest from "../api/makeRequest";
+import "../styles/_discover.scss";
 
 export default class Discover extends Component {
   constructor() {
@@ -12,15 +13,51 @@ export default class Discover extends Component {
       categories: []
     };
   }
-
+  async componentDidMount() {
+    const responseNewReleases = await makeRequest("new-releases").catch(
+      (err) => {
+        console.error(err);
+      }
+    );
+    const responsePlaylists = await makeRequest("featured-playlists").catch(
+      (err) => {
+        console.error(err);
+      }
+    );
+    const responseCategories = await makeRequest("categories").catch((err) => {
+      console.error(err);
+    });
+    console.log({ responseCategories });
+    const newReleases = responseNewReleases?.data;
+    const newPlaylists = responsePlaylists?.data;
+    const categories = responseCategories?.data;
+    console.log({
+      newReleases,
+      newPlaylists,
+      categories
+    });
+  }
   render() {
     const { newReleases, playlists, categories } = this.state;
 
     return (
       <div className="discover">
-        <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
-        <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
-        <DiscoverBlock text="BROWSE" id="browse" data={categories} imagesKey="icons" />
+        <DiscoverBlock
+          text="RELEASED THIS WEEK"
+          id="released"
+          data={newReleases}
+        />
+        <DiscoverBlock
+          text="FEATURED PLAYLISTS"
+          id="featured"
+          data={playlists}
+        />
+        <DiscoverBlock
+          text="BROWSE"
+          id="browse"
+          data={categories}
+          imagesKey="icons"
+        />
       </div>
     );
   }
